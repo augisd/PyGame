5c287f989c10db881663750a6eef8120f182e9c5
Augis <a.dankevicius@gmail.com>
2019-07-29 16:51:17 +0100

    skill started

--- playermodel.py
+++ playermodel.py
@@ -2,6 +2,7 @@ from tabulate import tabulate
 import pygame as pg
 #from game import Game
 from settings import *
+import time
 
 
 class PlayerType():
@@ -38,7 +39,7 @@ class PlayerType():
         self.update_skill()
 
         self.tendency -= self.tendency_modifier
-        self.skill -= self.skill_modifier
+        #self.skill -= self.skill_modifier
         if self.tendency < 0:
             self.tendency = 0
         if self.skill < 0:
@@ -49,13 +50,47 @@ class Explorer(PlayerType):
     # Coin pickup event increases tendency
     def __init__(self, game):
         PlayerType.__init__(self, game)
+        self.start_time = time.process_time()
+        self.coins_collected = 0
+        self.n_coins_avg = 5
+        self.avg_time_collection_size = 5
+        self.end_time = time.process_time()
+        self.time_elapsed = self.end_time - self.start_time
+        self.collection_times_log = []
+        self.skill = 0
 
     def update_tendency(self):
         if len(self.game.coins) < N_COINS:
             self.increase_tendency()
 
-    def update_skill(selfs):
-        pass
+    def update_skill(self):
+        if len(self.game.coins) < N_COINS:
+            self.coins_collected += 1
+            self.end_time = time.process_time()
+            self.time_elapsed = self.end_time - self.start_time
+            self.skill = self.coins_collected/self.time_elapsed
+
+    def update_skill2(self):
+
+        if len(self.game.coins) < N_COINS and self.coins_collected == self.n_coins_avg - 1:
+            self.end_time = time.process_time()
+            self.time_elapsed = self.end_time - self.start_time
+            self.start_time = time.process_time()
+
+            self.collection_times_log.append(self.time_elapsed)
+            if len(self.collection_times_log) > self.avg_time_collection_size:
+                self.collection_times_log = self.collection_times_log[1:]
+            self.skill = self.n_coins_avg  / (sum(self.collection_times_log) / len(self.collection_times_log))
+            self.coins_collected = 0
+
+            print(self.collection_times_log)
+
+        elif len(self.game.coins) < N_COINS and self.coins_collected < self.n_coins_avg:
+            self.coins_collected += 1
+
+        #print(self.time_elapsed)
+
+
 
 class Killer(PlayerType):
     # Enemy kill event increases killer tendency
